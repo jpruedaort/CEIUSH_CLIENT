c89ef66b9a24cd3e196ed7432b6fc60ca8c528ed
jpruedaort <jpruedaort@hotmail.com>
2021-11-28 22:05:51 -0500

    Todos los items estan enlasados con los estados. Costo total ya se puede obtener. Falta multiplicar por escalares correspondiente de cada indice. Ver planilla suministrada por la UISH

--- Frontend/ppsena/src/App.js
+++ Frontend/ppsena/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, createContext } from "react";
+import React, { useState, createContext, useEffect } from "react";
 import iushlogo from "./iushlogo.png";
 import "./App.css";
 import UserWindow from "./component/userWindow";
@@ -12,26 +12,124 @@ function App() {
   //Estados para abrir y cerrar las diferentes ventanas
   const [userInput, setUserInput] = useState(false); // Estado para crear nuevo usuario
   const [clientList, setclientList] = useState(false);
-  const [nombre, setNombre] = useState();
+  const [sumtotalBasicos, setSumtotalBasicos] = useState(0);
+  const [sumtotalGestion, setSumtotalGestion] = useState(0);
+  const [sumtotalOtros, setSumTotalOtros] = useState(0);
+  const [sumBasico, setSumBasico] = useState({
+    docencia: 0,
+    desarrollo: 0,
+    coordinacion: 0,
+    coordinaciod: 0,
+    coordinacioc: 0,
+    cartera: 0,
+    mercadeo: 0,
+    administrativo: 0,
+    cultivo: 0,
+    apoyo: 0,
+    juridica: 0,
+    vice: 0,
+  });
+  const [sumGestion, setSumGestion] = useState({
+    vicea: 0,
+    cont1: 0,
+    cont2: 0,
+    aux: 0,
+    tesoreria: 0,
+  });
+
+  const [sumOtros, setSumOtros] = useState({
+    aulaTeams: 0,
+    aulasist: 0,
+    aulasencilla: 0,
+    aulapost: 0,
+    auditorio: 0,
+    publicidad: 0,
+    costocert: 0,
+    transport: 0,
+    refri: 0,
+  });
+
+  //Actualiza el estado de  sumbasico cuando hay un cambio en el imput del grupo "basico"
+  const handleChangeBasico = (e) => {
+    const { name, value } = e.target;
+    setSumBasico((prevState) => ({
+      ...prevState,
+      [name]: value || 0,
+    }));
+  };
+
+  //Actualiza el estado de  sumGestion cuando hay un cambio en el imput del grupo "gestion"
+  const handleChangeGestion = (e) => {
+    const { name, value } = e.target;
+    setSumGestion((prevState) => ({
+      ...prevState,
+      [name]: value || 0,
+    }));
+  };
+
+  //Actualiza el estado de sumOtros cuando hay un cambio en el imput del grupo "otros"
+  const handleChangeOtros = (e) => {
+    const { name, value } = e.target;
+    setSumOtros((prevState) => ({
+      ...prevState,
+      [name]: value || 0,
+    }));
+    console.table(sumOtros);
+  };
 
   //para abrir o cerrar la ventana de nuevo usuario
   const toogleClient = (e) => {
     e.preventDefault();
     setUserInput(!userInput);
-    console.log("UserInput");
   };
 
   //Para abrir o cerrar la ventana de listas de clientes
   const toogleClientList = (e) => {
     e.preventDefault();
     setclientList(!clientList);
-    console.log("UserList");
   };
 
-  //cambia el nombre
-  const nombrehappy = (e) => {
-    setNombre(e.target.value);
-  };
+  // se usa useEffect para que cada vez que cambia el valor de input se haga la suma total
+
+  useEffect(() => {
+    // const sumtotbasico = (sumBasico) =>
+    //   Object.values(sumBasico).reduce((a, b) => a + b);
+    const sumtotbasico =
+      parseFloat(sumBasico.docencia) +
+      parseFloat(sumBasico.desarrollo) +
+      parseFloat(sumBasico.coordinacion) +
+      parseFloat(sumBasico.coordinaciod) +
+      parseFloat(sumBasico.coordinacioc) +
+      parseFloat(sumBasico.cartera) +
+      parseFloat(sumBasico.mercadeo) +
+      parseFloat(sumBasico.administrativo) +
+      parseFloat(sumBasico.cultivo) +
+      parseFloat(sumBasico.apoyo) +
+      parseFloat(sumBasico.juridica) +
+      parseFloat(sumBasico.vice);
+    setSumtotalBasicos(sumtotbasico);
+
+    const sumtotGestion =
+      parseFloat(sumGestion.vicea) +
+      parseFloat(sumGestion.cont1) +
+      parseFloat(sumGestion.cont2) +
+      parseFloat(sumGestion.aux) +
+      parseFloat(sumGestion.tesoreria);
+    setSumtotalGestion(sumtotGestion);
+
+    const constSumOtro =
+      parseFloat(sumOtros.aulaTeams) +
+      parseFloat(sumOtros.aulasist) +
+      parseFloat(sumOtros.aulasencilla) +
+      parseFloat(sumOtros.aulapost) +
+      parseFloat(sumOtros.auditorio) +
+      parseFloat(sumOtros.publicidad) +
+      parseFloat(sumOtros.costocert) +
+      parseFloat(sumOtros.transport) +
+      parseFloat(sumOtros.refri);
+
+    setSumTotalOtros(constSumOtro);
+  }, [sumBasico, sumGestion, sumOtros]);
 
   const value = { toogleClient, toogleClientList };
 
@@ -69,53 +167,113 @@ function App() {
               <div className="divider">
                 <div className="inputLine">
                   <label>Docencia: </label>
-                  <input onChange={(e) => nombrehappy(e)}></input>
+                  <input
+                    name="docencia"
+                    type="number"
+                    onChange={(e) => handleChangeBasico(e)}
+                    placeholder="0"
+                  ></input>
                 </div>
                 <div className="inputLine">
                   <label>Desarrollo de Contenido: </label>
-                  <input></input>
+                  <input
+                    name="desarrollo"
+                    type="number"
+                    onChange={(e) => handleChangeBasico(e)}
+                    placeholder="0"
+                  ></input>
                 </div>
                 <div className="inputLine">
                   <label>Coordinacion administrativa: </label>
-                  <input></input>
+                  <input
+                    name="coordinacion"
+                    type="number"
+                    onChange={(e) => handleChangeBasico(e)}
+                    placeholder="0"
+                  ></input>
                 </div>
                 <div className="inputLine">
                   <label>Coordinacion academica (decano): </label>
-                  <input></input>
+                  <input
+                    name="coordinaciod"
+                    type="number"
+                    onChange={(e) => handleChangeBasico(e)}
+                    placeholder="0"
+                  ></input>
                 </div>
                 <div className="inputLine">
                   <label>Coordinacion academica (Coordinador): </label>
-                  <input></input>
+                  <input
+                    name="coordinacioc"
+                    type="number"
+                    onChange={(e) => handleChangeBasico(e)}
+                    placeholder="0"
+                  ></input>
                 </div>
                 <div className="inputLine">
                   <label>Cartera: </label>
-                  <input></input>
+                  <input
+                    name="cartera"
+                    type="number"
+                    onChange={(e) => handleChangeBasico(e)}
+                    placeholder="0"
+                  ></input>
                 </div>
                 <div className="inputLine">
                   <label>Mercadeo y Ventas: </label>
-                  <input></input>
+                  <input
+                    name="mercadeo"
+                    type="number"
+                    onChange={(e) => handleChangeBasico(e)}
+                    placeholder="0"
+                  ></input>
                 </div>
                 <div className="inputLine">
                   <label>Administrativo Documental CAD: </label>
-                  <input></input>
+                  <input
+                    name="administrativo"
+                    type="number"
+                    onChange={(e) => handleChangeBasico(e)}
+                    placeholder="0"
+                  ></input>
                 </div>
                 <div className="inputLine">
                   <label>Cultivo de talento humano: </label>
-                  <input></input>
+                  <input
+                    name="cultivo"
+                    type="number"
+                    onChange={(e) => handleChangeBasico(e)}
+                    placeholder="0"
+                  ></input>
                 </div>
                 <div className="inputLine">
                   <label>Apoyo academico: </label>
-                  <input></input>
+                  <input
+                    name="apoyo"
+                    type="number"
+                    onChange={(e) => handleChangeBasico(e)}
+                    placeholder="0"
+                  ></input>
                 </div>
                 <div className="inputLine">
                   <label>Juridica: </label>
-                  <input></input>
+                  <input
+                    name="juridica"
+                    type="number"
+                    onChange={(e) => handleChangeBasico(e)}
+                    placeholder="0"
+                  ></input>
                 </div>
                 <div className="inputLine">
                   <label>
                     Vicerrectoria de planeacion y desarollo Organizacional{" "}
                   </label>
-                  <input></input>
+                  <input
+                    name="vice"
+                    type="number"
+                    onChange={(e) => handleChangeBasico(e)}
+                    placeholder="0"
+                  ></input>
                 </div>
               </div>
               <div className="groupnames">
@@ -124,24 +282,49 @@ function App() {
               <div className="divider">
                 <div className="inputLine">
                   <label>Vicerrectoria Administrativa y Financieras: </label>
-                  <input></input>
+                  <input
+                    name="vicea"
+                    type="number"
+                    onChange={(e) => handleChangeGestion(e)}
+                    placeholder="0"
+                  ></input>
                 </div>
 
                 <div className="inputLine">
                   <label>Contador 1: </label>
-                  <input></input>
+                  <input
+                    name="cont1"
+                    type="number"
+                    onChange={(e) => handleChangeGestion(e)}
+                    placeholder="0"
+                  ></input>
                 </div>
                 <div className="inputLine">
                   <label>Contador 2: </label>
-                  <input></input>
+                  <input
+                    name="cont2"
+                    type="number"
+                    onChange={(e) => handleChangeGestion(e)}
+                    placeholder="0"
+                  ></input>
                 </div>
                 <div className="inputLine">
                   <label>Auxiliar Contable: </label>
-                  <input></input>
+                  <input
+                    name="aux"
+                    type="number"
+                    onChange={(e) => handleChangeGestion(e)}
+                    placeholder="0"
+                  ></input>
                 </div>
                 <div className="inputLine">
                   <label>Tesoreria: </label>
-                  <input></input>
+                  <input
+                    name="tesoreria"
+                    type="number"
+                    onChange={(e) => handleChangeGestion(e)}
+                    placeholder="0"
+                  ></input>
                 </div>
               </div>
               <div className="groupnames">
@@ -150,39 +333,84 @@ function App() {
               <div className="divider">
                 <div className="inputLine">
                   <label>Aula por Teams</label>
-                  <input></input>
+                  <input
+                    name="aulaTeams"
+                    type="number"
+                    onChange={(e) => handleChangeOtros(e)}
+                    placeholder="0"
+                  ></input>
                 </div>
                 <div className="inputLine">
                   <label>Aula de sistemas</label>
-                  <input></input>
+                  <input
+                    name="aulasist"
+                    type="number"
+                    onChange={(e) => handleChangeOtros(e)}
+                    placeholder="0"
+                  ></input>
                 </div>
                 <div className="inputLine">
                   <label>Aula sencilla</label>
-                  <input></input>
+                  <input
+                    name="aulasencilla"
+                    type="number"
+                    onChange={(e) => handleChangeOtros(e)}
+                    placeholder="0"
+                  ></input>
                 </div>
                 <div className="inputLine">
                   <label>Aula postgrado (Especiales): </label>
-                  <input></input>
+                  <input
+                    name="aulapost"
+                    type="number"
+                    onChange={(e) => handleChangeOtros(e)}
+                    placeholder="0"
+                  ></input>
                 </div>
                 <div className="inputLine">
                   <label>Auditorio: </label>
-                  <input></input>
+                  <input
+                    name="auditorio"
+                    type="number"
+                    onChange={(e) => handleChangeOtros(e)}
+                    placeholder="0"
+                  ></input>
                 </div>
                 <div className="inputLine">
                   <label>Publicidad </label>
-                  <input></input>
+                  <input
+                    name="publicidad"
+                    type="number"
+                    onChange={(e) => handleChangeOtros(e)}
+                    placeholder="0"
+                  ></input>
                 </div>
                 <div className="inputLine">
                   <label>Costo certificion: </label>
-                  <input></input>
+                  <input
+                    name="costocert"
+                    type="number"
+                    onChange={(e) => handleChangeOtros(e)}
+                    placeholder="0"
+                  ></input>
                 </div>
                 <div className="inputLine">
                   <label>Transporte y alimentación: </label>
-                  <input></input>
+                  <input
+                    name="transport"
+                    type="number"
+                    onChange={(e) => handleChangeOtros(e)}
+                    placeholder="0"
+                  ></input>
                 </div>
                 <div className="inputLine">
                   <label>Refrigerios: </label>
-                  <input></input>
+                  <input
+                    name="refri"
+                    type="number"
+                    onChange={(e) => handleChangeOtros(e)}
+                    placeholder="0"
+                  ></input>
                 </div>
               </div>
               <div className="inputLine" id="btnLine">
@@ -190,14 +418,19 @@ function App() {
                   {" "}
                   + Agregar Parametro{" "}
                 </button>
-                <h1>{nombre}</h1>
               </div>
             </form>
           </div>
           <div className=" rightmaincol col-4  p-0">
             <div className="totalcost">
-              <h2>Costo total</h2>
-              <h3>123312 COP</h3>
+              <h2>Costo:</h2>
+              <h3>Basicos: {sumtotalBasicos} COP</h3>
+              <h3>Gestion: {sumtotalGestion} COP </h3>
+              <h3>Otros: {sumtotalOtros} COP</h3>
+              <br />
+              <h3>Total:</h3>
+              <h2>{sumtotalBasicos + sumtotalGestion + sumtotalOtros}</h2>
+              <h2>COP</h2>
               <button> Genera Cotizacion</button>
             </div>
           </div>
